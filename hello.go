c2e9b2d81426e98b42c2bd94719e493da0cf086c
Luke-J-Miller <106999018+ljmbm5@users.noreply.github.com>
2022-08-12 12:10:08 -0500

    Tests and cases added for different languages

--- hello.go
+++ hello.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case "spanish", "Spanish":
+		prefix = spanishHelloPrefix
+	case "french", "French":
+		prefix = frenchHelloPrefix
+	}
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
