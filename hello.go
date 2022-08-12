997a857b3f46041d08df884f8c06bafb5cd968e4
Luke-J-Miller <106999018+ljmbm5@users.noreply.github.com>
2022-08-12 12:15:36 -0500

    Finished. split out greetingPrefix func

--- hello.go
+++ hello.go
@@ -10,15 +10,19 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "spanish", "Spanish":
 		prefix = spanishHelloPrefix
 	case "french", "French":
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name
+	return
 }
 
 func main() {
